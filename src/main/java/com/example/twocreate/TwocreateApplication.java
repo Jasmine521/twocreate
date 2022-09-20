f444d9546bcc3888ec107967a6669b6f3c089f36
Jasmine521 <1779605057@qq.com>
2022-09-20 10:00:41 +0800

    出生

---
+++ src/main/java/com/example/twocreate/TwocreateApplication.java
@@ -0,0 +1,13 @@
+package com.example.twocreate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwocreateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TwocreateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/twocreate/TwocreateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.twocreate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwocreateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
