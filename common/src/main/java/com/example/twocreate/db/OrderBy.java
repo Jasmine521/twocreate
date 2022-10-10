9eb86ccd69f128b5485bb62e31e2cbf3a77bedd0
Jasmine521 <1779605057@qq.com>
2022-10-10 16:03:54 +0800

    ORM

---
+++ common/src/main/java/com/example/twocreate/db/OrderBy.java
@@ -0,0 +1,42 @@
+package com.example.twocreate.db;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public final class OrderBy<T> extends CriteriaQuery<T> {
+    public OrderBy(Criteria<T> criteria, String orderBy) {
+        super(criteria);
+        orderBy(orderBy);
+    }
+    public OrderBy<T> orderBy(String orderBy) {
+        if (criteria.orderBy == null) {
+            criteria.orderBy = new ArrayList<>();
+        }
+        criteria.orderBy.add(orderBy);
+        return this;
+    }
+
+
+    public OrderBy<T> desc(){
+        int last = this.criteria.orderBy.size() -1;
+        String s = criteria.orderBy.get(last);
+        if (!s.toUpperCase().endsWith(" DESC")) {
+            s = s + " DESC";
+        }
+        criteria.orderBy.set(last, s);
+        return this;
+    }
+    public Limit<T> limit(int maxResults) {
+        return limit(0, maxResults);
+    }
+
+    public Limit<T> limit(int offset, int maxResults) {
+        return new Limit<>(this.criteria,offset,maxResults);
+    }
+    public List<T> list() {
+        return criteria.list();
+    }
+    public T first(){
+        return criteria.first();
+    }
+}
